24b085909f4830e3d629d62979fc3cfea5ae8576
Trpl7ca <52380252+Trpl7ca@users.noreply.github.com>
2024-08-27 16:45:56 -0700

    Update LoopConstants.swift

--- Loop/Models/LoopConstants.swift
+++ Loop/Models/LoopConstants.swift
@@ -42,7 +42,7 @@ enum LoopConstants {
         HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 60)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 200)
 
     static let glucoseChartDefaultDisplayBoundClamped =
-        HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 80)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 240)
+        HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 45)...HKQuantity(unit: .milligramsPerDeciliter, doubleValue: 270)
     
     
     // Compile time configuration
